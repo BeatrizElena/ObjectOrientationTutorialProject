9d215ae30b793d4cff6f08cc56fed2e65797e23d
Bea Ortiz <ortizwebdevelopment@gmail.com>
2018-01-09 14:46:10 -0500

    Make new SaloonCar class that inherits from Car class. Add new property: numberOfSeats.

---
+++ Car/Car/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car("red", "Toyota");
+        }
+    }
+
+    public class Car
+    {
+        public Car(string _color, string _manufacturer)
+        {
+            this.color = _color;
+            this.manufacturer = _manufacturer;
+        }
+
+        public string color { get; set; }
+        public string manufacturer { get; set; }
+    }
+        public class SaloonCar : Car
+        {
+            public SaloonCar ()
+            {
+            }
+            public int numberOfSeats { get; set; }
+        }
+}

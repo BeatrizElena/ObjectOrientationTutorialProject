6d531119214a07cac552ca81f749db4e6b72ffd3
Bea Ortiz <ortizwebdevelopment@gmail.com>
2018-01-09 15:32:33 -0500

    Create 3 constructors that inherit from each other. Create 3 salonCar objects, each with different properties.

--- Car/Car/Program.cs
+++ Car/Car/Program.cs
@@ -7,25 +7,63 @@ namespace Car
         static void Main(string[] args)
         {
             var car1 = new Car("red", "Toyota");
+            var saloonCar1 = new SaloonCar1(2);
+            var saloonCar2 = new SaloonCar2(3, "Nissan");
+            var saloonCar3 = new SaloonCar3(4, "Ford", "purple");
         }
     }
 
-    public class Car
+    // Construct for property common to all objects: numberOfSeats
+    public class CommonConstructs
+    {
+        public CommonConstructs()
+        {
+        }
+        public int numberOfSeats { get; set; }
+    }
+
+    //Construct2 for numberOfSeats and manufacturer
+    public class Constructor2 : CommonConstructs
+    {
+        public Constructor2 ()
+        {
+        }
+        public string _manufacturer { get; set; }
+    }
+
+    //Construct3 for numberOfSeats, manufacturer and color
+    public class Constructor3 : Constructor2
+    {
+        public Constructor3 ()
+        {
+        }
+        public string _color { get; set; }
+    }
+    public class Car : Constructor3
     {
         public Car(string _color, string _manufacturer)
         {
-            this.color = _color;
-            this.manufacturer = _manufacturer;
         }
-
-        public string color { get; set; }
-        public string manufacturer { get; set; }
     }
-        public class SaloonCar : Car
+
+    public class SaloonCar1 : CommonConstructs
+    {
+        public SaloonCar1(int num)
         {
-            public SaloonCar ()
-            {
-            }
-            public int numberOfSeats { get; set; }
         }
+    }
+
+    public class SaloonCar2 : Constructor2
+    {
+        public SaloonCar2(int num, string _manufacturer)
+        {
+        }
+    }
+
+    public class SaloonCar3 : Constructor3
+    {
+        public SaloonCar3(int num, string _color, string _manufacturer)
+        {
+        }
+    }
 }
